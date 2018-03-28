190c5436d2eaa39d17ce9217e547180c3131fb71
TranTrungHieu <hieu.trantrung1204@icloud.com>
2018-03-28 19:21:11 +0700

    + implement fastbuild_android (TBD)

--- Tools/FastBuild/helloworld/fastbuild_android.bat
+++ Tools/FastBuild/helloworld/fastbuild_android.bat
@@ -5,5 +5,7 @@ set CUR_PATH=%~dp0
 set FASTBUILD_PATH=%CUR_PATH%..\
 
 echo Fastbuild in: %FASTBUILD_PATH%
+echo.
+echo.
 call %FASTBUILD_PATH%\FBuild.exe -config %CUR_PATH%\android\fbuild.bff
 pause
