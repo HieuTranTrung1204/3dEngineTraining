e560925f4594f6f1b0ef7198f2f44e9b37285406
TranTrungHieu <hieu.trantrung1204@icloud.com>
2018-03-29 16:41:02 +0700

    * refactor fastbuild & improve helloworld FastBuild

--- Tools/FastBuild/helloworld/android/build_o.bat
+++ Tools/FastBuild/helloworld/android/build_o.bat
@@ -8,7 +8,7 @@ set src_cpp=%CUR_PATH%\..
 set src_build=%src_cpp%\native-lib.cpp
 
 set out_build_o=%src_cpp%\android\Out\native-lib.o
-set out_build_so=%src_cpp%\android\Out\native-lib.so
+REM set out_build_so=%src_cpp%\android\Out\native-lib.so
 
 
 call %ndk_clang% -I%ndk_path%\sysroot\usr\include -I%ndk_path%\sources\cxx-stl\llvm-libc++\include -I%ndk_path%\platforms\android-21\arch-arm\usr\include -target armv7-none-linux-androideabi -c %src_build% -o %out_build_o%
