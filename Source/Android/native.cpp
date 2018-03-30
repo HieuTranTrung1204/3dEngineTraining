1b75742dbda862d1793eba0cd33f197b20f262c5
TranTrungHieu <hieu.trantrung1204@icloud.com>
2018-03-30 13:29:20 +0700

    * refactor build win32 & android

---
+++ Source/Android/native.cpp
@@ -0,0 +1,11 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_hieu_com_a3dengine_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hieu zz Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
